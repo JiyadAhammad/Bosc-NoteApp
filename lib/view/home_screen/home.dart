983a57743289692d652d2c60e226530601ca36fb
JiyadAhammad <jiyadahammad99@gmail.com>
2022-09-20 18:19:37 +0530

    grid view and app bar generated

--- lib/view/home_screen/home.dart
+++ lib/view/home_screen/home.dart
@@ -1,10 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/model/constant/color/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (context, value) {
+          return [
+            SliverAppBar(
+              snap: true,
+              floating: true,
+              expandedHeight: 150,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                expandedTitleScale: 1.8,
+                background: Container(
+                  // height: MediaQuery.of(context).size.height * 0.7,
+                  // width: MediaQuery.of(context).size.width * 0.9,
+                  // // Below is the code for Linear Gradient.
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.purple,
+                        Colors.blue,
+                      ],
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                    ),
+                  ),
+                ),
+                title: const Text(
+                  'NoteApp',
+                  style: TextStyle(
+                    color: kwhite,
+                    fontSize: 25,
+                  ),
+                ),
+                centerTitle: true,
+              ),
+            ),
+          ];
+        },
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 4 / 4,
+          ),
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(
+                  color: kblack,
+                  style: BorderStyle.solid,
+                  width: 2.5,
+                ),
+                color: Colors.transparent,
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          size: 35,
+        ),
+      ),
+    );
   }
 }
